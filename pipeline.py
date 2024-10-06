10bade4300226a867c95930c6aeb6a4e325d0199
Astarag Mohapatra <40949756+Athe-kunal@users.noreply.github.com>
2024-10-06 11:04:16 -0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -7,11 +7,13 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import collections
 from groq import AsyncGroq
-from groq import Groq
-
+import pydantic
+from typing import Annotated
 
 QueryImgTuple = collections.namedtuple("QueryImgTuple",['query','image_base64'])
 
+class EnrichedQuery(pydantic.BaseModel):
+    enriched_queries: Annotated[str,"List of enriched queries separated "]
 class VisionFinancePipeLine(dspy.Module):
     def __init__(self):
         self.vision_index = self._get_vision_index()
@@ -51,8 +53,8 @@ class VisionFinancePipeLine(dspy.Module):
         # return SummaryResponse.model_validate_json(chat_completion.choices[0].message.content)
         return completion.choices[0].message.content
     
-    def query_translator(self,user_query):
-        completion = self.groq_client.chat.completions.create(
+    async def query_translator(self,user_query):
+        completion = await self.groq_client.chat.completions.create(
         messages=[
             {
                 "role": "system",
@@ -67,8 +69,9 @@ class VisionFinancePipeLine(dspy.Module):
     )
         return completion.choices[0].message.content
     
-    def query_enrichment(self, user_query, query_translator, summaries):
-        chat_completion = self.groq_client.chat.completions.create(
+    async def query_enrichment(self, user_query, query_translator, summaries):
+        
+        chat_completion = await self.groq_client.chat.completions.create(
         messages=[
             {
                 "role": "system",
@@ -83,39 +86,50 @@ class VisionFinancePipeLine(dspy.Module):
     )
         return chat_completion.choices[0].message.content
     
-    def manager_response(self, manager_response_list, query_translator, user_query):
-        """
-        @param manager_response_list: list of responses from the manager agent
-        This is typically a List[str] such that each element is a text summary
-        of each image.
-        @param query_translator: A refined and thoroughly explicit and detailed
-        version of the original user query.
-        @param user_query: the original user query
-        """
+    async def manager_response(self, manager_response_list, query_translator, user_query):
         summaries_with_ids = [f"Summary {i}: {summary}" for i, summary in enumerate(manager_response_list)]
         summaries_text = "\n\n".join(summaries_with_ids)
         
-        chat_completion = self.groq_client.chat.completions.create(
+        chat_completion = await self.groq_client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content": "You are an expert in evaluating the relevance of information to user queries. Your task is to analyze a list of summaries and determine which ones are most relevant to the user's original query and the translated query. Return only the IDs of the most relevant summaries, separated by commas."
+                    "content": f"You are an expert in evaluating the relevance of information to user queries. Your task is to analyze a list of summaries and determine which ones are most relevant to the user's original query and the translated query. Return only the IDs of the most relevant summaries as JSON object {json.dumps(EnrichedQuery.model_json_schema())}, separated by commas."
                 },
                 {
                     "role": "user",
-                    "content": f"User query: '{user_query}'\nTranslated query: '{query_translator}'\n\nSummaries:\n{summaries_text}\n\nPlease provide the IDs of the most relevant summaries, separated by commas."
+                    "content": f"User query: '{user_query}'\nTranslated query: '{query_translator}'\n\nSummaries:\n{summaries_text}\n\nPlease provide the IDs of the most relevant summaries as JSON object separated by commas."
+                }
+            ],
+            model="llama-3.1-70b-versatile",
+            response_format={"type": "json_object"},
+        )
+        
+        relevant_summary_ids = EnrichedQuery.model_validate_json(chat_completion.choices[0].message.content)
+        return relevant_summary_ids
+    
+    async def summarize_final_response(self,relevant_response_list, query_translator, user_query):
+        summaries_with_ids = [f"Summary {i}: {summary}" for i, summary in enumerate(relevant_response_list)]
+        summaries_text = "\n\n".join(summaries_with_ids)
+        
+        chat_completion = await self.groq_client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are given a list of enumerated summaries. Based on the user question, your task is to summarize all the provided summaries. Make sure that your answer is relevant to the user query."
+                },
+                {
+                    "role": "user",
+                    "content": f"User query: '{user_query}'\nTranslated query: '{query_translator}'\n\nSummaries:\n{summaries_text}\n\n. Answer: "
                 }
             ],
             model="llama-3.1-70b-versatile",
         )
+        return chat_completion.choices[0].message.content.strip()
 
-        relevant_summary_ids = chat_completion.choices[0].message.content.strip()
-        return [int(id.strip()) for id in relevant_summary_ids.split(',')]
-    
-    
     async def __call__(self,user_query:str):
         #Translate the simple user query to better query
-        query_translator = self.query_translator(user_query)
+        query_translator = await self.query_translator(user_query)
         
         relevant_summaries = self.summary_index.invoke(query_translator)
         
@@ -124,12 +138,12 @@ class VisionFinancePipeLine(dspy.Module):
             summaries+=rs.page_content + "\n\n"
         
         #Based on relevant summaries, it translates the user query into three enriched queries
-        enriched_queries = self.query_enrichment(user_query, query_translator, summaries)
-        
+        enriched_queries = await self.query_enrichment(user_query, query_translator, summaries)
+        enriched_queries = enriched_queries.split("\n\n")
         relevant_img_results: list[QueryImgTuple] = []
         
         for eq in enriched_queries:
-            relevant_imgs = self.vision_index.search(query=eq,k=2)
+            relevant_imgs = self.vision_index.search(query=eq,k=1)
             relevant_img_results.append(
                 QueryImgTuple(query=eq,image_base64=[(i['base64'], await self.groq_response(i['base64'],eq)) for i in relevant_imgs])
             )
@@ -137,12 +151,20 @@ class VisionFinancePipeLine(dspy.Module):
         manager_response_list:list[str] = []
         for ri in relevant_img_results:
             for r in ri.image_base64:
+                #append the second index
                 manager_response_list.append(
                     r[1]
                 )
         #Manager response
-        manager_response = self.manager_response(manager_response_list,query_translator, user_query)
-        return query_translator, summaries, relevant_img_results, manager_response
+        relevant_response = await self.manager_response(manager_response_list,query_translator, user_query)
+        relevant_response_list = []
+        relevant_ids = [r.strip() for r in relevant_response.enriched_queries.split(",")]
+        for rp in relevant_ids:
+            relevant_response_list.append(
+                manager_response_list[int(rp)]
+            )
+        final_response = await self.summarize_final_response(relevant_response_list, query_translator, user_query)
+        return query_translator, summaries, relevant_img_results, relevant_response_list, manager_response_list, final_response
     
 def _get_summary_index(path):
     model_name = "sentence-transformers/all-mpnet-base-v2"
